333a70b7ef7a16e939acfdd2d5d01543c5270442
Dan Carr <pdxdiver@users.noreply.github.com>
2017-04-14 22:05:38 -0700

    Update docker-entrypoint.sh

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PATH=$PATH:~/.local/bin
 ./bin/getconfig.sh
-python manage.py migrate --noinput
+#python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-  gunicorn homelessAPI.wsgi:application -b :8000
+gunicorn homelessAPI.wsgi:application -b :8000 -k 'gevent' --keep-alive 60
